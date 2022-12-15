48c8b670221d9f87a1a56d5cefe131e9c01fe772
biotechcommie <hugocarlos.bg@gmail.com>
2022-12-15 15:51:49 -0300

    application with CLI and setup.py update

--- acppred/predict.py
+++ acppred/predict.py
@@ -1,12 +1,41 @@
 from acppred.models import Model
+from argparse import ArgumentParser
+import pandas as pd
+from Bio import SeqIO
+
 
 def main():
-    model = Model.load('data/models/model.pickle')
-    sequence = input('peptide sequence: ')
-    prediction = model.predict(sequence)
+    argument_parser = ArgumentParser(description='Predicts anticancer peptides in a FASTA file')
+    argument_parser.add_argument(
+        '--input',
+        required=True,
+        help='input FASTA file'
+    )
+    argument_parser.add_argument(
+        '--output',
+        required=True,
+        help='output CSV file'
+    )
+    argument_parser.add_argument(
+        '--model',
+        required=True,
+        help='Pre-trained ACPPred model'
+    )
+    arguments = argument_parser.parse_args()
+    
+    model = Model.load(arguments.model)
+    
+    predictions = []
 
-    print(prediction)
+    for sequence_record in SeqIO.parse(arguments.input, 'fasta'):
+        sequence = str(sequence_record.seq)
+        prediction = model.predict(sequence)
+        sequence_data = {'sequence_id': sequence_record.id, 'prediction': prediction}
+        predictions.append(sequence_data)
 
-# verificar quantidade de _______ neste trecho, com o doc do fred
+    df_predictions = pd.DataFrame(predictions, columns=['sequence_id', 'prediction'])
+    df_predictions.to_csv(arguments.output, index=False)
+
+# igual em train.py
 if __name__ == '__main__':
     main()
